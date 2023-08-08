9470407587af8c6d7337eabebd7d44758928fc0d
Davide-Caputo <davidestinsoncaputo08@gmail.com>
2023-08-08 16:27:33 +0200

    navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- google font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet">
     <title>Document</title>
     @livewireStyles
     @vite('resources/css/app.css')
 </head>
-<body class="bg-info">
+<body>
     
 <x-navbar />
 
