31a5bafe3286582dc70d233e49cdb81bc9ece3ac
Michele Ercolini <ercolinimichele@outlook.it>
2023-08-08 15:18:17 +0200

    livewire FormLogIn

---
+++ app/Http/Livewire/FormLogIn.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class FormLogIn extends Component
+{
+    public function render()
+    {
+        return view('livewire.form-log-in');
+    }
+}
