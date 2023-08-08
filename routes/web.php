d12f5502ef56a3d15e8a67b398f03e91fcaebf1e
Michele Ercolini <ercolinimichele@outlook.it>
2023-08-08 17:56:33 +0200

    Rotta controller vista, crea annuncio

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PublicController;
+use App\Models\Annuouncement;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('homepage');
+Route::get('/',[PublicController::class, 'home'])->name('homepage');
+
+//Rotta create announcement
+Route::get('/announcement/create', [Annuouncement::class, 'create'])->name('announement_create');
