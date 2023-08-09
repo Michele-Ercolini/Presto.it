57086fd7fcd674c2aca3fd1467f9c263493a0721
Davide-Caputo <davidestinsoncaputo08@gmail.com>
2023-08-09 12:24:17 +0200

    stylecard

--- resources/views/announcement/index.blade.php
+++ resources/views/announcement/index.blade.php
@@ -1,36 +1,22 @@
 <x-layout>
-<<<<<<< HEAD
-
-    <div class=" ">
-=======
     
     <div class="containerlogin ">
->>>>>>> b2d3f9754af4f40c9c10f4a4508f59c60bea5a1a
         <div class="row justify-content-center">
             <div class="col-12-col-md-3">
                 <h1>I NOSTRI ANNUNCI</h1>
             </div>
         </div>
     </div>
-<<<<<<< HEAD
-
-    <div class="containerx d-flex justify-content-center">
-        <div class="card">
-            <div class="content">
-                <h2>01</h2>
-                <h3 style="color:black">Card one</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam explicabo voluptatum suscipit facere maiores, doloremque, consequatur vitae tempore distinctio quis magni repudiandae quaerat blanditiis numquam! Sunt facere porro in asperiores!</p>
-                <a href="">Read more</a>
-=======
     
     <div class="container">
         <div class="row">
             
             @foreach($announcements as $announcement)
             <div class="col col-md-4">
-                <div class="">
+                <div class="containerx">
                     <div class="card">
                         <div class="content">
+                            <img src="https://picsum.photos/250" height="200px" width="200px">
                             <h2>Titolo: {{$announcement->title}}</h2>
                             <h3>Categoria:{{$announcement->category->name}}</h3>
                             <p class="text-truncate">{{$announcement->body}}</p>
@@ -38,7 +24,6 @@
                         </div>
                     </div>
                 </div>
->>>>>>> b2d3f9754af4f40c9c10f4a4508f59c60bea5a1a
             </div>
             @endforeach
             
