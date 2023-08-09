dd8fcaa10e6e93cf910bde14ff24be0534f7b7b4
MariusDobreanu <dobreanucatalinmarius@gmail.com>
2023-08-09 12:27:34 +0200

    tolto titolo in card

--- resources/views/announcement/index.blade.php
+++ resources/views/announcement/index.blade.php
@@ -17,7 +17,7 @@
                     <div class="card">
                         <div class="content">
                             <img src="https://picsum.photos/250" height="200px" width="200px">
-                            <h2>Titolo: {{$announcement->title}}</h2>
+                            <h2>{{$announcement->title}}</h2>
                             <h3>Categoria:{{$announcement->category->name}}</h3>
                             <p class="text-truncate">{{$announcement->body}}</p>
                             <a href="{{route('announcement_detail', compact('announcement'))}}">Read more</a>
