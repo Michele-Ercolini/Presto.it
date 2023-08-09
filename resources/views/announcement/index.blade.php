adf06d8b02c223e5f5655cd65644348560eaf69c
Davide-Caputo <davidestinsoncaputo08@gmail.com>
2023-08-09 10:38:26 +0200

    card

---
+++ resources/views/announcement/index.blade.php
@@ -0,0 +1,31 @@
+<x-layout>
+
+    <div class="containerlogin ">
+        <div class="row justify-content-center">
+            <div class="col-12-col-md-3">
+                <h1>ANNUNCI</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="containerx d-flex justify-content-center">
+        <div class="card">
+            <div class="content">
+                <h2>01</h2>
+                <h3>Card one</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam explicabo voluptatum suscipit facere maiores, doloremque, consequatur vitae tempore distinctio quis magni repudiandae quaerat blanditiis numquam! Sunt facere porro in asperiores!</p>
+                <a href="">Read more</a>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+</x-layout>
