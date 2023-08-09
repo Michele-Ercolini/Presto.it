b35b8750a4436c728a41fe4dbcd10991643f5f68
Davide-Caputo <davidestinsoncaputo08@gmail.com>
2023-08-09 15:36:35 +0200

    card modificata

--- resources/views/announcement/index.blade.php
+++ resources/views/announcement/index.blade.php
@@ -11,9 +11,9 @@
                         <div class="content">
                             <img class="img" src="https://picsum.photos/250" height="200px" width="200px">
                             <h2>{{$announcement->title}}</h2>
-                            <h3>Categoria: {{$announcement->category->name}}</h3>
+                            {{-- <h3>Categoria: {{$announcement->category->name}}</h3> --}}
                             <p class="text-truncate">{{$announcement->body}}</p>
-                            <a class="btn1" href="{{route('announcement_detail', compact('announcement'))}}">Read more</a>
+                            <a class="btn1" style="margin-bottom:50px" href="{{route('announcement_detail', compact('announcement'))}}">Read more</a>
                         </div>
                     </div>
                 </div>
