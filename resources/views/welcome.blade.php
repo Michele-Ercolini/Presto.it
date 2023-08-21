d2a37ec018a49310755663fc77abf9e3a28ae3fc
Michele Ercolini <ercolinimichele@outlook.it>
2023-08-21 17:18:23 +0200

    merge

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,17 +30,12 @@
         <div class="row justify-content-center">
             <div class="col-12 col-md-6">
                 <h3 class="text-center paragrafo">Benvenuti nel nostro elegante negozio online!</h3>
-<<<<<<< HEAD
-                <p class="text-center"> Scopri uno straordinario mondo di stile e tendenze con il nostro esclusivo sito e-commerce.</p>
-                <h3 class="text-center paragrafo display-3">I nostri Annunci più recenti</h3>
-=======
                 <p class="text-center" > Scopri uno straordinario mondo di stile e tendenze con il nostro esclusivo sito e-commerce.</p>
                 <video loop autoplay muted width="100%" height="360">
                     <source src="/media/Vecchietta.mp4" type="video/mp4">
                 </video>
 
             
->>>>>>> c518a8b783b3c15709d3150b30c41ad047f8611b
             </div>
         </div>
     </div>
