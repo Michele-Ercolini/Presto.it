a1b251b6dae7f1812b6e5a2f2f08f77719181b71
Michele Ercolini <ercolinimichele@outlook.it>
2023-08-21 09:44:40 +0200

    modifiche generali

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,7 +41,6 @@
     
             </div>
         </div>
-    </div>
     </header>
 
 </x-layout>
--- resources/views/user/lavora.blade.php
+++ resources/views/user/lavora.blade.php
@@ -1,11 +1,21 @@
 <x-layout>
 
     <header class="container-fluid bgimg d-flex align-items-center justify-content-center">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="text-white display-1">Lavora con noi</h1>
-                <h3>Vuoi diventare un nostro Revisore?</h3>
-                <a class="btn1 btn search-button btn-outline-dark" href="{{route('become_revisor')}}">Clicca qui</a>
+        <div class="row align-items-center justify-content-center">
+            
+
+                @if(Auth::user()->is_revisor)
+                    <div class="col-12">
+                        <h1 class="text-white display-1 text-center">Ciao {{Auth::user()->name}}</h1>
+                        <h3 class="text-center">Sei già un nostro Revisore</h3>
+                    </div>
+                @else
+                    <div class="col-12">
+                        <h1 class="text-white display-1 text-center">Lavora con noi</h1>
+                        <h3 class="text-center">Vuoi diventare un nostro Revisore?</h3>
+                        <a class="btn1 btn btn-outline-dark" href="{{route('become_revisor')}}">Clicca qui</a>
+                    </div>                
+                @endif
                     
                 @if (session('message'))
                     <div class="alert alert-success">
