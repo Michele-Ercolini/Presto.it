aec99eb1328b794fd211555388af3e86ff2b66df
Davide Cutrona <d.cutrona85@gmail.com>
2023-08-23 13:10:52 +0200

    mergeeee

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -2,24 +2,14 @@
 
 namespace App\Jobs;
 
-<<<<<<< HEAD
-=======
 use Spatie\Image\Image;
->>>>>>> 7f5b75e38dd1304befbe84e30af20d54b1d91325
 use Illuminate\Bus\Queueable;
 use Spatie\Image\Manipulations;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-<<<<<<< HEAD
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
-use Spatie\Image\Manipulations;
-use Spatie\Image\Image;
-=======
 use Illuminate\Contracts\Queue\ShouldBeUnique;
->>>>>>> 7f5b75e38dd1304befbe84e30af20d54b1d91325
 
 class ResizeImage implements ShouldQueue
 {
