17a130272d15cfbd09bd37f497419e90db914538
AntonioPaparella <npaparella20@gmail.com>
2023-08-23 10:29:11 +0200

    ES language

---
+++ lang/es/ui.php
@@ -0,0 +1,18 @@
+<?php
+return[
+    'annunci'=>'Anuncios',
+    'profilo'=>'Hola usuario/a',
+    'categorie'=>'Categorías',
+    'cerca'=>'Buscar',
+    'introduzione'=>'¡Bienvenido a nuestra elegante tienda de moda!',
+    'semi-introduzione'=>'Descubra un extraordinario mundo de estilo y tendencias con nuestro exclusivo sitio de comercio electrónico.',
+    'nostri-annunci'=>'Nuestros anuncios más recientes',
+    'titolo1'=>'Ofertas irresistibles:',
+    'descrizione1'=>' Buscamos constantemente ofertas ventajosas que te permitan ahorrar en tus compras favoritas. Desde rebajas de temporada hasta promociones exclusivas, te garantizamos prendas de alta costura y accesorios coleccionables. Somos el destino para todo lo que es inalcanzable, únase a nosotros para descubrir productos únicos y raros que no encontrará en ningún otro lugar.',
+    'articolo2'=>'para que los clientes se registren, beneficios exclusivos, promociones y la oportunidad de vender sus productos mediante la creación de anuncios. Haga clic en (Hola Usuario) para registrarse y completar el breve formulario con su información básica. Inicie sesión en su nueva cuenta y comience a explorar el mundo de productos de alta calidad, ofertas extraordinarias y comience a cargar sus productos.',
+    'titolo2'=>'Registro:',
+    'titolo3'=>'Únase a nuestro equipo:',
+    'articolo3'=>'consulte nuestra sección trabaja con nosotros. Únase a nuestro equipo como revisor y ayúdenos a dar forma a la experiencia de nuestros clientes. Ingrese al mundo detrás de escena del comercio electrónico, ponga a prueba su buen ojo para los detalles y ayude a garantizar la calidad de nuestros productos.',
+    'counter1'=>'Ventas diarias',
+    'counter2'=>"Las visitas de hoy",
+];
