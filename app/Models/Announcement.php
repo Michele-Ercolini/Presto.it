645647863728563c571c836f8386ac8bb2264620
Davide Cutrona <d.cutrona85@gmail.com>
2023-08-24 16:51:16 +0200

    Searchbar risolto

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
+use App\Models\Category;
 
 class Announcement extends Model
 {
