3052771c5be5cf0c3de32eb0dc7c41e172e20b9c
Davide Cutrona <d.cutrona85@gmail.com>
2023-08-25 00:14:23 +0200

    User Story 7

---
+++ app/Jobs/GoogleVisionSafeSearch.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Image;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class GoogleVisionSafeSearch implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $announcement_image_id;
+    /**
+     * Create a new job instance.
+     * @return void
+     */
+    public function __construct($announcement_image_id)
+    {
+        $this->announcement_image_id = $announcement_image_id;
+    }
+
+    /**
+     * Execute the job.
+     * 
+     * @return void
+     */
+    public function handle()
+    {
+        $i = Image::find($this->announcement_image_id);
+        if (!$i) {
+            return;
+        }
+
+        $image = file_get_contents(storage_path('app/public/' . $i->path));
+
+        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('2022-04-26-presto-livewire-user-story-7.json'));
+
+        $imageAnnotator = new ImageAnnotatorClient();
+        $response = $imageAnnotator->safeSearchDetection($image);
+        $imageAnnotator->close();
+
+        $safe = $response->getSafeSearchAnnotation();
+
+        $adult = $safe->getAdult();
+        $medical = $safe->getMedical();
+        $spoof = $safe->getSpoof();
+        $violence = $safe->getViolence();
+        $racy = $safe->getRacy();
+
+        $likelihoodName = [
+            'text-secondary fas fa-circle', 'text-success fas fa-circle', 'text-success fas fa-circle',
+            'text-warning fas fa-circle', 'text-warning fas fa-circle', 'text-warning fas fa-circle',
+        ];
+
+        $i->adult = $likelihoodName[$adult];
+        $i->medical = $likelihoodName[$medical];
+        $i->spoof = $likelihoodName[$spoof];
+        $i->violence = $likelihoodName[$violence];
+        $i->racy = $likelihoodName[$racy];
+
+        $i->save();
+    }
+}
