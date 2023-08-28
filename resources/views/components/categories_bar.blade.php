788c12d7df9da3dd8c984a1ba5a16b00dbd75245
AntonioPaparella <npaparella20@gmail.com>
2023-08-28 10:52:09 +0200

    Media query categories, video

--- resources/views/components/categories_bar.blade.php
+++ resources/views/components/categories_bar.blade.php
@@ -1,8 +1,34 @@
-<div class="row ">
-    <div class="categorie col-12">
+<div class="row">
+    <div class="categorie categoriePa col-12">
         {{-- @dd($categories) --}}
         @foreach ($categories as $category)
             <a class="cat" href="{{route('category_show', compact('category'))}}"> | {{$category->name}} | - </a>
         @endforeach
     </div>
 </div>
+
+<style>
+@media (max-width: 1920px) {
+    .categoriePa{
+        padding-left: 250px;
+    }
+  }
+
+  @media (max-width: 1400px) {
+    .categoriePa{
+        padding-left: 180px;
+    }
+  }
+
+  @media (max-width: 1200px) {
+    .categoriePa{
+        padding-left: 80px;
+    }
+  }
+
+  @media (max-width: 990px) {
+    .categoriePa{
+        padding-left: 30px;
+    }
+  }
+</style>
